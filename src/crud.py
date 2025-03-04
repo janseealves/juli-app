0c1c0eacea4f19fd923fee70833877a17a2c92f7
jansee <jansen.alvesr@gmail.com>
2025-03-04 16:01:57 -0300

    Implement user model, database setup, and CRUD operations with FastAPI routes

---
+++ src/crud.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from .models import User
+from .schemas import UserSchema, UserPublic
+
+def get_users(db: Session):
+    return db.query(User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def create_user(db: Session, user: UserSchema):
+    db_user = UserSchema(**user)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,25 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from http import HTTPStatus
+from sqlalchemy.orm import Session
 
-from app.schemas import UserPublic, UsersResponse
+from src.schemas import UserPublic, UsersResponse, UserSchema
+from src.crud import get_users, get_user, create_user
+from src.database import Base, engine, SessionLocal
 
 router = APIRouter()
 
-users = [
-    {
-        "id": 1,
-        "name": "John Doe",
-        "email": "johndoe@example.com",
-        "password": "secret"
-    },
-    {
-        "id": 2,
-        "name": "Jane Doe",
-        "email": "janedoe@example.com",
-        "password": "secret"
-    }
-]
+Base.metadata.create_all(bind=engine)
 
-@router.get("/users",tags=["user"], status_code=HTTPStatus.OK, response_model=UsersResponse)
-def get_user():
-    return {"users": users}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/user",tags=["user"], status_code=HTTPStatus.OK, response_model=UsersResponse)
+def get_users_endpoint(db: Session=Depends(get_db)):
+    return get_users(db)
+
+@router.get("/user/{user_id}",tags=["user"], status_code=HTTPStatus.OK, response_model=UserPublic)
+def get_user_endpoint(user_id: int, db: Session=Depends(get_db)):
+    return get_user(db, user_id)
+
+@router.post("/user", tags=["user"], status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user_endpoint(user: UserSchema, db: Session=Depends(get_db)):
+    return create_user(db, user)
