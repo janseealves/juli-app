69bd16e267dbcdaf8947403974d3b4b076201921
jansee <jansen.alvesr@gmail.com>
2025-03-04 22:55:27 -0300

    Add models for categories, expenses, incomes, wallets, and wallet_users

--- src/models.py
+++ src/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import DATE
 
-registry = registry()
+registry = registry() # Registra metadados de classes mapeadas
 
 @registry.mapped_as_dataclass
-
 class User:
     __tablename__ = "users"
 
@@ -11,3 +11,44 @@ class User:
     name: Mapped[str]
     email: Mapped[str]
     password: Mapped[str]
+
+class Category:
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str] = mapped_column(not_null=True)
+
+class Expense:
+    __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    user_id: Mapped[int] = mapped_column(foreign_key=User.id, not_null=True)
+    amount: Mapped[float] = mapped_column(not_null=True)
+    description: Mapped[str]
+    date: Mapped[DATE] = mapped_column(not_null=True) #TODO: Verificar se é possível usar DATE
+    category_id: Mapped[int] = mapped_column(foreign_key=Category.id, not_null=True)
+
+class Income:
+    __tablename__ = "incomes"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    user_id: Mapped[int] = mapped_column(foreign_key=User.id, not_null=True)
+    amount: Mapped[float] = mapped_column(not_null=True)
+    description: Mapped[str]
+    date: Mapped[DATE] = mapped_column(not_null=True) #TODO: Verificar se é possível usar DATE
+
+class Wallet:
+    __tablename__ = "wallets"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    user_id: Mapped[int] = mapped_column(foreign_key=User.id, not_null=True)
+    balance: Mapped[float] = mapped_column(not_null=True)
+    last_update: Mapped[DATE] = mapped_column(not_null=True) #TODO: Verificar se é possível usar DATE
+
+class Wallet_User:
+    __tablename__ = "wallet_users"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    user_id: Mapped[int] = mapped_column(foreign_key=User.id, not_null=True)
+    wallet_id: Mapped[int] = mapped_column(foreign_key=Wallet.id, not_null=True)
+    is_owner: Mapped[bool] = mapped_column(not_null=True)
--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, EmailStr
 
+class Message(BaseModel):
+    message: str
+
 class UserSchema(BaseModel):
     name: str
     email: EmailStr
